e3c7ec91711b5f889df1e831457253c15cc9037f
Tymianek <tymi@tymi.org>
2025-06-12 19:10:43 +0200

    User/tymus/lista prodoktow (#76)
    * products API ig
    
    * enforce authorisation
    
    * frontend API for getting the product list

--- FrontEnd/auth.js
+++ FrontEnd/auth.js
@@ -35,6 +35,15 @@ const API = {
         if (!response.ok) throw new Error(json);
 
         return json;
+    },
+    GetProductList: async () => {
+        if (API.accessToken === null) throw new Error("Access token was null");
+
+        const response = await API.AuthGet("/api/products.php");
+        const json = await response.json();
+        if (!response.ok) throw new Error(json);
+
+        return json;
     }
 };
 
