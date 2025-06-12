dac0705c79f3ffcdd91e298b044f5e9cc1518cac
TymianekPL <tymi@tymi.org>
2025-06-12 19:08:04 +0200

    enforce authorisation

--- api/products.php
+++ api/products.php
@@ -7,15 +7,15 @@ header("Content-Type: application/json");
 
 $token = trim(getallheaders()["Authorization"] ?? "");
 
-// if (empty($token)) {
-//     HttpUtils::Status(401, "Unauthorised");
-// }
+if (empty($token)) {
+    HttpUtils::Status(401, "Unauthorised");
+}
 
 ReloadEnvFile();
 $db = new Database($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
 
 $token = Database::getInstance()->real_escape_string($token);
 
-//$account = GetAccountOrDie($token);
+$account = GetAccountOrDie($token);
 
 echo json_encode(Products::all());
