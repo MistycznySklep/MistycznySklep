64ca490a4bab6b38e9f58f0a43bc448d892833dd
TymianekPL <tymi@tymi.org>
2025-06-12 19:07:45 +0200

    products API ig

---
+++ api/products.php
@@ -0,0 +1,21 @@
+<?php
+require_once "misc.php";
+require_once "Models/Accounts.php";
+require_once "Models/Products.php";
+
+header("Content-Type: application/json");
+
+$token = trim(getallheaders()["Authorization"] ?? "");
+
+// if (empty($token)) {
+//     HttpUtils::Status(401, "Unauthorised");
+// }
+
+ReloadEnvFile();
+$db = new Database($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+
+$token = Database::getInstance()->real_escape_string($token);
+
+//$account = GetAccountOrDie($token);
+
+echo json_encode(Products::all());
