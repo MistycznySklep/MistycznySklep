d9f3715dcbfa68a9e3bca6437b3ffbe0ebb0ddf6
Tymianek <tymi@tymi.org>
2025-06-16 19:43:31 +0200

    Update 8_product_categories.sql (#98)
    * Update 8_product_categories.sql
    
    * Update 11_categories_edit.sql
    
    * Update 13_cart.sql

--- db/11_categories_edit.sql
+++ db/11_categories_edit.sql
@@ -4,7 +4,7 @@ idProduct_subcategories INT(11) NOT NULL;
 CREATE TABLE `categories` (
   `idCategories` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `category` varchar(50) NOT NULL
-) 
+);
 
 INSERT INTO `categories` (`idCategories`, `category`) VALUES
 (1, 'Rośliny'),
