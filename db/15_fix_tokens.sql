c1e909f0a011dd4c80283b13f000a75f84c29a52
Tymianek <tymi@tymi.org>
2025-06-17 17:16:55 +0200

    Rename 15_fix_tokens.sql to 17_fix_tokens.sql

--- db/15_fix_tokens.sql
+++
@@ -1,3 +0,0 @@
-alter table login_token drop constraint fk_accounts_login_token;
-
-ALTER TABLE login_token ADD CONSTRAINT fk_token_account FOREIGN KEY (idAccounts) REFERENCES accounts(idAccounts) ON DELETE CASCADE;
