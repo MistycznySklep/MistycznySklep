75b9dcb686204e4ad3addd3f223db321b6215b7c
TymianekPL <tymi@tymi.org>
2025-06-17 20:51:58 +0200

    Add unique index to MFA

--- db/20_mfa.sql
+++ db/20_mfa.sql
@@ -1,2 +1,4 @@
 create table awaiting_mfas (id int primary key auto_increment, token text not null, idAccounts int not null, generated_at datetime not null default current_timestamp());
 alter table awaiting_mfas add constraint fk_awaiting_mfas_accounts foreign key (idAccounts) references accounts(idAccounts);
+alter table awaiting_mfas add unique index uq_mfa_token (token);
+alter table awaiting_mfas add unique index uq_mfa_uniqueAccount (idAccounts);
