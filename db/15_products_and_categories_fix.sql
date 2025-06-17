6d6a5b7b4f788c710a1af247e9361a7b6dc27994
naaimjeeed <106909998+naaimjeeed@users.noreply.github.com>
2025-06-17 16:42:05 +0200

    dodanie poprawek do produktów

---
+++ db/15_products_and_categories_fix.sql
@@ -0,0 +1,3 @@
+UPDATE products SET name = 'Magiczna Doniczka' WHERE products.idProducts = 17;
+UPDATE categories SET category = 'Wyposażenia' WHERE categories.idCategories = 4;
+UPDATE `products` SET `idProduct_subcategories` = '6' WHERE `products`.`idProducts` = 2; 
