156ce501f82ce9d057f86658f99b5cdfcfc9eee2
TymianekPL <tymi@tymi.org>
2025-06-17 20:47:43 +0200

    fix db

--- db/20_mfa.sql
+++ db/20_mfa.sql
@@ -1,2 +1,2 @@
-create table awaiting_mfas (id int primary key auto_increment, token text not null, idAccounts int not null);
+create table awaiting_mfas (id int primary key auto_increment, token text not null, idAccounts int not null, generated_at datetime not null default current_timestamp());
 alter table awaiting_mfas add constraint fk_awaiting_mfas_accounts foreign key (idAccounts) references accounts(idAccounts);
