34f882a9bcb10845d0d05c31bd8be9adeb889bab
Tymianek <tymi@tymi.org>
2025-06-17 23:53:44 +0200

    dodawanie chyba nie wiem czy dziala (#152)

--- api/products.php
+++ api/products.php
@@ -18,4 +18,42 @@ $token = Database::getInstance()->real_escape_string($token);
 
 $account = GetAccountOrDie($token);
 
+if (
+    isset($_POST["name"]) &&
+    isset($_POST["johncena"]) &&
+    isset($_POST["kolor"]) &&
+    isset($_POST["description"]) &&
+    isset($_POST["quantity"]) &&
+    isset($_POST["cat"]) &&
+    isset($_POST["image"])
+) {
+    $data = $_POST["image"];
+
+    if (preg_match('/^data:image\/(\w+);base64,/', $data, $matches)) {
+        $ext = $matches[1];
+        $data = substr($data, strpos($data, ',') + 1);
+        $data = base64_decode($data);
+
+        if ($data === false) {
+            die('Base64 decoding failed');
+        }
+        $sql = "insert into imgs values (NULL, 'meow.png')";
+        $res = Database::getInstance()->query($sql);
+        $id = Database::getInstance()->insert_id;
+
+        $fileName = "$id.png";
+        $filePath = __DIR__ . "/userImages/$fileName";
+
+        file_put_contents($filePath, $data);
+        $sql = "insert into products values (NULL, ?, ?, ?, ?, null, ?, ?, $id);";
+        $stmt = Database::getInstance()->prepare($sql);
+        $stmt->bind_param("siiss", $_POST["name"], $_POST["cat"], $_POST["johncena"], $_POST["description"], $_POST["quantity"], $_POST["kolor"]);
+        $stmt->execute();
+        $stmt->close();
+    } else {
+        die('Invalid image data format');
+    }
+}
+
+
 echo json_encode(Products::all($_GET["q"] ?? ""));
