a2cce85ebc88fd414fe9e5e1ea0f080a817d15df
TymianekPL <tymi@tymi.org>
2025-06-18 10:57:19 +0200

    string

--- api/products.php
+++ api/products.php
@@ -47,7 +47,7 @@ if (
         file_put_contents($filePath, $data);
         $sql = "insert into products values (NULL, ?, ?, ?, ?, null, ?, ?, $id, ?);";
         $stmt = Database::getInstance()->prepare($sql);
-        $stmt->bind_param("siissi", $_POST["name"], $_POST["cat"], $_POST["johncena"], $_POST["description"], $_POST["quantity"], $_POST["kolor"], $_POST["cat"]);
+        $stmt->bind_param("ssissi", $_POST["name"], $_POST["cat"], $_POST["johncena"], $_POST["description"], $_POST["quantity"], $_POST["kolor"], $_POST["cat"]);
         $stmt->execute();
         $stmt->close();
     } else {
