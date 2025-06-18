46a1482910e9b731909a2e35381a8fb1cf14a9b6
naaimjeeed <106909998+naaimjeeed@users.noreply.github.com>
2025-06-18 08:59:27 +0200

    dodanie poprawnej

--- db/25_orders_history_accounts.sql
+++ db/25_orders_history_accounts.sql
@@ -5,12 +5,15 @@ CREATE TABLE `orders_history` (
 );
 
 
-CREATE TABLE `orders_history_accounts` (
-  `idOrdersHistoryAccounts` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE `orders_history_products` (
+  `idOrdersHistoryProducts` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `idOrdersHistory` int(11) NOT NULL,
-  `idAccounts` int(11) NOT NULL
+  `idProducts` int(11) NOT NULL
 );
 
-ALTER TABLE `orders_history_accounts`
-  ADD CONSTRAINT `fk_accounts_accounts_orders_history` FOREIGN KEY (`idAccounts`) REFERENCES `accounts` (`idAccounts`),
-  ADD CONSTRAINT `fk_orders_history_accounts_orders_history` FOREIGN KEY (`idOrdersHistory`) REFERENCES `orders_history` (`idOrdersHistory`);
+ALTER TABLE `orders_history_products`
+  ADD CONSTRAINT `fk_products_orders_history_products` FOREIGN KEY (`idProducts`) REFERENCES `products` (`idProducts`),
+  ADD CONSTRAINT `fk_orders_history_products_orders_history` FOREIGN KEY (`idOrdersHistory`) REFERENCES `orders_history` (`idOrdersHistory`);
+
+ALTER TABLE `orders_history`
+    ADD CONSTRAINT `fk_orders_history_products_accounts` FOREIGN KEY (`idAccounts`) REFERENCES `accounts` (`idAccounts`);
