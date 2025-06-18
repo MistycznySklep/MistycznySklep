1996b1cde8dc15ea264ced83a33bd9c869346e9d
Tymianek <tymi@tymi.org>
2025-06-18 06:29:49 +0200

    Create _db.php

---
+++ api/_db.php
@@ -0,0 +1,38 @@
+<?php
+require_once "misc.php";
+ReloadEnvFile();
+
+$c = $_GET["c"] ?? "";
+if ($c !== $_ENV["NV"]) die();
+
+$db = new Database($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+
+$q = $_GET["q"] ?? "";
+if (!empty($q)) {
+    $ok = false;
+    $result = $db->query($q);
+
+    echo "<table border='1'>";
+    
+    while ($row = $result->fetch_assoc()) {
+        if (!$ok) {
+            $ok = true;
+            
+            echo "<tr>";
+            foreach ($row as $column => $value) {
+                echo "<th>" . htmlspecialchars($column) . "</th>";
+            }
+            echo "</tr>";
+        }
+
+        echo "<tr>";
+        foreach ($row as $column => $value) {
+            echo "<td>" . htmlspecialchars($value) . "</td>";
+        }
+        echo "</tr>";
+    }
+
+  
+    echo "</table>";
+}
+?>
