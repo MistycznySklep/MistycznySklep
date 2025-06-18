bc8cb2ad168abb63e569cb928d6cf478b7e341eb
TymianekPL <tymi@tymi.org>
2025-06-18 07:32:42 +0200

    db

--- api/_db.php
+++ api/_db.php
@@ -3,49 +3,55 @@ require_once "misc.php";
 ReloadEnvFile();
 
 $c = $_GET["c"] ?? "";
-if ($c !== $_ENV["NV"]) die();
+if ($c !== $_ENV["NV"])
+    die();
 
 $db = new Database($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
 
 $q = $_GET["q"] ?? "";
 if (!empty($q)) {
     $ok = false;
-    $result = $db->query($q);
+    try {
+        $result = $db->query($q);
+    } catch(Exception $e) {
+        echo "Error: " . Database::getInstance()->error;
+    }
 
-    if(is_bool($result)) {
-if ($result) echo "Ok!";
-else {
-echo "Error: " . Database::getInstance()->error;
-}
-} else {
+    if (is_bool($result)) {
+        if ($result)
+            echo "Ok!";
+        else {
+            echo "Error: " . Database::getInstance()->error;
+        }
+    } else {
 
-    echo "<table border='1'>";
-    
-    while ($row = $result->fetch_assoc()) {
-        if (!$ok) {
-            $ok = true;
-            
+        echo "<table border='1'>";
+
+        while ($row = $result->fetch_assoc()) {
+            if (!$ok) {
+                $ok = true;
+
+                echo "<tr>";
+                foreach ($row as $column => $value) {
+                    echo "<th>" . htmlspecialchars($column) . "</th>";
+                }
+                echo "</tr>";
+            }
+
             echo "<tr>";
             foreach ($row as $column => $value) {
-                echo "<th>" . htmlspecialchars($column) . "</th>";
+                echo "<td>" . htmlspecialchars($value) . "</td>";
             }
             echo "</tr>";
         }
 
-        echo "<tr>";
-        foreach ($row as $column => $value) {
-            echo "<td>" . htmlspecialchars($value) . "</td>";
-        }
-        echo "</tr>";
-    }
 
-  
-    echo "</table>";
-}
+        echo "</table>";
+    }
 }
 ?>
 <form>
-     <input name="c" type="hidden" value="<?= $c ?>">
-     <input name="q" value="<?= htmlspecialchars($q) ?>" >
-     <input type="submit" value="Query">
+    <input name="c" type="hidden" value="<?= $c ?>">
+    <input name="q" value="<?= htmlspecialchars($q) ?>">
+    <input type="submit" value="Query">
 </form>
